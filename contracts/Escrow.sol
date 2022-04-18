850cdd2f44c47f78139b88f2d712c045509b8a76
Yinha <466880466@qq.com>
2022-04-18 23:59:44 +0800

    更新合约

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,7 +1,6 @@
 pragma solidity ^ 0.5.16;
 
 contract Escrow {
- uint public productId; // 产品id
  address public buyer; //买家
  address public seller; //卖家
  address public arbiter;  //仲裁
@@ -12,18 +11,17 @@ contract Escrow {
  mapping (address => bool) refundAmount;
  uint public refundCount;
 
- event CreateEscrow(uint _productId, address _buyer, address _seller, address _arbiter);
- event UnlockAmount(uint _productId, string _operation, address _operator);
- event DisburseAmount(uint _productId, uint _amount, address _beneficiary);
+ event CreateEscrow(address _buyer, address _seller, address _arbiter);
+ event UnlockAmount(string _operation, address _operator);
+ event DisburseAmount(uint _amount, address _beneficiary);
 
- constructor(uint _productId, address _buyer, address _seller, address _arbiter) payable public {
-  productId = _productId;
+ constructor(address _buyer, address _seller, address _arbiter) payable public {
   buyer = _buyer;
   seller = _seller;
   arbiter = _arbiter;
   amount = msg.value;
   fundsDisbursed = false;
-  emit CreateEscrow(_productId, _buyer, _seller, _arbiter);
+  emit CreateEscrow(_buyer, _seller, _arbiter);
  }
 
  function escrowInfo() view public returns (address, address, address, bool, uint, uint) {
@@ -35,28 +33,27 @@ contract Escrow {
   if ((caller == buyer || caller == seller || caller == arbiter) && releaseAmount[caller] != true) {
    releaseAmount[caller] = true;
    releaseCount += 1;
-   emit UnlockAmount(productId, "release", caller);
+   emit UnlockAmount("release", caller);
   }
 
   if (releaseCount == 2) {
    address(uint160(seller)).transfer(amount);
    fundsDisbursed = true;
-   emit DisburseAmount(productId, amount, seller);
+   emit DisburseAmount(amount, seller);
   }
  }
 
- function refundAmountToBuyer(address caller) public {
+ function refundAmountToBuyer(address caller) payable public {
   require(!fundsDisbursed);
   if ((caller == buyer || caller == seller || caller == arbiter) && refundAmount[caller] != true) {
    refundAmount[caller] = true;
    refundCount += 1;
-   emit UnlockAmount(productId, "refund", caller);
+   emit UnlockAmount("refund", caller);
   }
-
   if (refundCount == 2) {
    address(uint160(buyer)).transfer(amount);
    fundsDisbursed = true;
-   emit DisburseAmount(productId, amount, buyer);
+   emit DisburseAmount(amount, buyer);
   }
  }
 }
